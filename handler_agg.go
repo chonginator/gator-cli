372b875d01b7296fb73e6ac142fc02cfe11e74c0
chonginator <benchong1999@yahoo.com.au>
2024-10-05 10:46:14 +1000

    cleanup agg

--- handler_agg.go
+++ handler_agg.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"html"
 )
 
 func handlerAggregate(state *state, cmd command) error {
@@ -13,14 +12,6 @@ func handlerAggregate(state *state, cmd command) error {
 		return fmt.Errorf("couldn't fetch feed: %w", err)
 	}
 
-	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
-	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
-
-	for i, item := range feed.Channel.Items {
-		feed.Channel.Items[i].Description = html.UnescapeString(item.Description)
-		feed.Channel.Items[i].Title = html.UnescapeString(item.Title)
-	}
-
-	fmt.Printf("%+v", feed)
+	fmt.Printf("Feed: %+v", feed)
 	return nil
 }
--- rss_feed.go
+++ rss_feed.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"encoding/xml"
 	"fmt"
+	"html"
 	"io"
 	"net/http"
+	"time"
 )
 
 type RSSFeed struct{
@@ -13,7 +15,7 @@ type RSSFeed struct{
 			Title string				`xml:"title"`
 			Link string					`xml:"link"`
 			Description string	`xml:"description"`
-			Items []RSSItem			`xml:"item"`
+			Item []RSSItem			`xml:"item"`
 		} `xml:"channel"`
 }
 
@@ -25,33 +27,49 @@ type RSSItem struct{
 }
 
 func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
 	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
 	if err != nil {
-		return &RSSFeed{}, err
+		return nil, err
 	}
 
-	req.Header.Add("User-Agent", "gator")
-
-	client := http.Client{}
+	req.Header.Set("User-Agent", "gator")
 	res, err := client.Do(req)
 	if err != nil {
-		return &RSSFeed{}, err
+		return nil, err
 	}
 	if res.StatusCode > 399 {
-		return &RSSFeed{}, fmt.Errorf("non-OK status code: %v", res.StatusCode)
+		return nil, fmt.Errorf("non-OK status code: %v", res.StatusCode)
 	}
 	defer res.Body.Close()
 
 	data, err := io.ReadAll(res.Body)
 	if err != nil {
-		return &RSSFeed{}, err
+		return nil, err
 	}
 
-	rssFeed := &RSSFeed{}
+	var rssFeed RSSFeed
 	err = xml.Unmarshal(data, &rssFeed)
 	if err != nil {
-		return &RSSFeed{}, err
+		return nil, err
 	}
 
-	return rssFeed, nil
+	rssFeed = decodeRSSHTMLEntities(rssFeed)
+
+	return &rssFeed, nil
+}
+
+func decodeRSSHTMLEntities(rssFeed RSSFeed) RSSFeed {
+	rssFeed.Channel.Title = html.UnescapeString(rssFeed.Channel.Title)
+	rssFeed.Channel.Description = html.UnescapeString(rssFeed.Channel.Description)
+
+	for i, item := range rssFeed.Channel.Item {
+		item.Title = html.UnescapeString(item.Title)
+		item.Description = html.UnescapeString(item.Description)
+		rssFeed.Channel.Item[i] = item
+	}
+	
+	return rssFeed
 }
