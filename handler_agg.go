56e4747ca3170d07970c7d64d0d021526e6c0a48
chonginator <benchong1999@yahoo.com.au>
2024-10-05 13:20:37 +1000

    cleanup feed handler

--- handler_agg.go
+++ handler_agg.go
@@ -3,10 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
-
-	"github.com/chonginator/gator-cli/internal/database"
-	"github.com/google/uuid"
 )
 
 func handlerAggregate(state *state, cmd command) error {
@@ -17,36 +13,5 @@ func handlerAggregate(state *state, cmd command) error {
 	}
 
 	fmt.Printf("Feed: %+v", feed)
-	return nil
-}
-
-func handlerAddFeed(state *state, cmd command) error {
-	if len(cmd.args) != 2 {
-		return fmt.Errorf("usage: addfeed <name> <url>")
-	}
-
-	name, url := cmd.args[0], cmd.args[1]
-
-	user, err := state.db.GetUser(context.Background(), state.cfg.CurrentUserName)
-	if err != nil {
-		return fmt.Errorf("couldn't get current user to add feed: %w", err)
-	}
-
-	feed, err := state.db.CreateFeed(context.Background(), database.CreateFeedParams{
-		ID: uuid.New(),
-		CreatedAt: time.Now().UTC(),
-		UpdatedAt: time.Now().UTC(),
-		Name: name,
-		Url: url,
-		UserID: user.ID,
-	})
-	if err != nil {
-		return fmt.Errorf("couldn't add feed: %w", err)
-	}
-
-	fmt.Printf(" * ID: %v\n", feed.ID)
-	fmt.Printf(" * Name: %v\n", feed.Name)
-	fmt.Printf(" * URL: %v\n", feed.Url)
-	 
 	return nil
 }
