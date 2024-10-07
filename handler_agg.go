0aefc49c7fb3f68dd6af2cc698af34c0c6fe59d8
chonginator <benchong1999@yahoo.com.au>
2024-10-07 15:14:46 +1000

    cleanup feed scraper

--- handler_agg.go
+++ handler_agg.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/chonginator/gator-cli/internal/database"
@@ -14,51 +14,42 @@ func handlerAggregate(state *state, cmd command) error {
 		return fmt.Errorf("usage: %s <time_between_reqs>", cmd.name)
 	}
 
-	time_between_reqs, err := time.ParseDuration(cmd.args[0])
+	timeBetweenRequests, err := time.ParseDuration(cmd.args[0])
 	if err != nil {
-		return fmt.Errorf("couldn't parse time_between_reqs duration string: %w", err)
+		return fmt.Errorf("invalid duration string: %w", err)
 	}
 
-	fmt.Printf("Collecting feeds every %s\n", time_between_reqs)
+	log.Printf("Collecting feeds every %s...", timeBetweenRequests)
 
-	ticker := time.NewTicker(time_between_reqs)
+	ticker := time.NewTicker(timeBetweenRequests)
+
 	for ; ; <-ticker.C {
-		err := scrapeFeeds(state.db)
-		if err != nil {
-			return fmt.Errorf("couldn't scrape feed: %w", err)
-		}
+		scrapeFeeds(state.db)
 	}
 }
 
-func scrapeFeeds(db *database.Queries) error {
+func scrapeFeeds(db *database.Queries) {
 	nextFeed, err := db.GetNextFeedToFetch(context.Background())
 	if err != nil {
-		return fmt.Errorf("couldn't get next feed to fetch: %w", err)
+		log.Println("Couldn't get next feeds to fetch", err)
+		return
 	}
+	log.Println("Found a feed to fetch!")
 
-	err = db.MarkFeedFetched(context.Background(), database.MarkFeedFetchedParams{
-		ID: nextFeed.ID,
-		LastFetchedAt: sql.NullTime{
-			Time: time.Now().UTC(),
-			Valid: true,
-		},
-	})
+	err = db.MarkFeedFetched(context.Background(), nextFeed.ID)
 	if err != nil {
-		return fmt.Errorf("couldn't mark next feed as fetched: %w", err)
+		log.Printf("Couldn't mark next feed %s fetched: %v", nextFeed.Name, err)
+		return
 	}
-	fmt.Printf("%s feed successfully marked as fetched!\n", nextFeed.Name)
 
-	fmt.Printf("fetching %s...", nextFeed.Url)
 	rssFeed, err := fetchFeed(context.Background(), nextFeed.Url)
 	if err != nil {
-		return fmt.Errorf("couldn't fetch next feed: %w", err)
+		log.Printf("Couldn't collect feed %s: %v", nextFeed.Name, err)
+		return
 	}
-	fmt.Printf("%s fetched successfully!\n", nextFeed.Url)
 
-	fmt.Println("Posts:")
 	for _, item := range rssFeed.Channel.Item {
-		fmt.Printf("* %s\n", item.Title)
+		fmt.Printf("Found post: %s\n", item.Title)
 	}
-
-	return nil
+	log.Printf("Feed %s collected, %v posts found", nextFeed.Name, len(rssFeed.Channel.Item))
 }
